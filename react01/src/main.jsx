12ff3667b3491cb64af4528f53b5d18a81229e61
Shubham-K-Mane <shubhammane0552@gmail.com>
2023-12-10 17:10:37 +0530

    testing

--- react01/src/main.jsx
+++ react01/src/main.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import Name from './test.jsx'
+//import Name from './test.jsx'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
   root.render(
-  <>
-      <App />
-      <Name />
-  </>
-  
+
+      <App />  
+
 )
