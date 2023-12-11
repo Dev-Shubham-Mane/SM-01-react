b4ab765cc581acc7a7c360de656a85a22ce95b98
Shubham-K-Mane <shubhammane0552@gmail.com>
2023-12-11 23:21:35 +0530

    feat
    leaned hooks in react

--- react02/src/App.jsx
+++ react02/src/App.jsx
@@ -5,11 +5,36 @@ import './App.css'
 
 function App() {
   //const [count, setCount] = useState(0)
+  let [counter, setCount]  = useState(10)
+  const addOne = () => {
+      //console.log(counter)
+      if(counter >= 21){
+          return counter    
+      } else{
+        setCount(counter++)
+      }
+      
+  }
+
+  const minusOne = () => {
+    if(counter <=0 ){
+      return counter
+    }else{
+      setCount(counter--)
+    }
+
+  }
 
   return (
     <>
       <h1> Hello World </h1>
       <h2> Hello India </h2>
+
+      
+      <button onClick={addOne} >Add One : {counter}</button>
+      <br />
+      <br />
+      <button onClick={minusOne} >Minus One : {counter}</button>
     </>
   )
 }
